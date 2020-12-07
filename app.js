cace1babd7c3c5b9ad01bb91f0050217894381d4
dushyant2901 <74450982+dushyant2901@users.noreply.github.com>
2020-12-07 23:37:39 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,4 @@
+var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("#btn");
+var output = document.querySelector("#output");
+btnTranslate.addEventListener("click",clicked)
